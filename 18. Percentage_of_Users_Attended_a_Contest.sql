18f725b9689dc1c550fad140f9580bae3e897da0
yuvarajmk <66544265+yuvarajmk@users.noreply.github.com>
2024-09-16 18:37:29 -0700

    18. Percentage_of_Users_Attended_a_Contest.sql

--- 18. Percentage_of_Users_Attended_a_Contest.sql
+++ 18. Percentage_of_Users_Attended_a_Contest.sql
@@ -10,27 +10,3 @@ select f.contest_id,   round(count(r.user_id)*100/count(f.user_id),2) percentage
 from full_table f left join register r on (f.user_id = r.user_id and f.contest_id = r.contest_id)
 group by f.contest_id
 order by 2 desc, 1 
----
-with u_cnt as (select count(user_id) cnt from users )
-select contest_id, round(count(r.user_id)/min(u_cnt.cnt)*100,2) as percentage
-from register r left join u_cnt on (1=1)
-group by contest_id
-order by percentage desc, contest_id
-SELECT 
-  contest_id, -- The ID of the contest
-  ROUND(
-    COUNT(DISTINCT user_id) * 100 / ( -- Calculate the percentage of users
-      SELECT 
-        COUNT(user_id) -- Total number of unique users
-      FROM 
-        Users
-    ), 
-    2
-  ) AS percentage -- The percentage of users registered for each contest, rounded to 2 decimal places
-FROM 
-  Register -- The table containing registration information
-GROUP BY 
-  contest_id -- Group the data by contest ID
-ORDER BY 
-  percentage DESC, -- Order the results by percentage in descending order
-  contest_id; -- Then order by contest ID for ties
